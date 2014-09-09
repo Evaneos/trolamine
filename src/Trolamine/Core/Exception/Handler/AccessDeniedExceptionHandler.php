a85997467a31250bcd474412cc3af4819a1c3f05
Steven Fouquet <steven@evaneos.com>
2014-09-09 10:18:52 +0200

    Handled 401

--- src/Trolamine/Core/Exception/Handler/AccessDeniedExceptionHandler.php
+++ src/Trolamine/Core/Exception/Handler/AccessDeniedExceptionHandler.php
@@ -2,14 +2,22 @@
 namespace Trolamine\Core\Exception\Handler;
 
 use \Pyrite\Exception\ExceptionHandler;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+use Trolamine\Core\Authentication\AnonymousAuthenticationToken;
+use Trolamine\Layer\AuthenticationLayer;
 
 class AccessDeniedExceptionHandler implements ExceptionHandler
 {
 
     public function handleException(\Exception $exception, \Pyrite\Response\ResponseBag $responseBag)
     {
-        $responseBag->setResultCode(403);
+        $authentication = $responseBag->get(AuthenticationLayer::VAR_NAME);
 
-        return $responseBag;
+        if ($authentication === null || $authentication instanceof AnonymousAuthenticationToken) {
+            throw new HttpException(401, $exception->getMessage(), $exception);
+        }
+
+        throw new AccessDeniedHttpException($exception->getMessage(), $exception);
     }
 }
